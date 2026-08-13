b133237b46247dbdeb0f0507052e55be5c38dfdd
Prathmeshnavale1098 <prathmeshnavale900@gmail.com>
2026-08-13 12:25:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ from flask_mail import Mail, Message
 app = Flask(__name__)
 app.secret_key = "focus123"
 
-app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD)
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
+app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 
-  
 mail = Mail(app)
+
 
 @app.route("/")
 def home():
